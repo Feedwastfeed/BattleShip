c56599d221de976ebeb61d788715404e834c99cf
mostafa <mostafa36ahmed@gmail.com>
2021-12-07 20:27:01 +0200

    Add Project

---
+++ BattleShip/Help.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace BattleShip
+{
+    partial class Help
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Help));
+            this.label1 = new System.Windows.Forms.Label();
+            this.AA10 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Arial", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Silver;
+            this.label1.Location = new System.Drawing.Point(10, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(431, 184);
+            this.label1.TabIndex = 1;
+            this.label1.Text = resources.GetString("label1.Text");
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // AA10
+            // 
+            this.AA10.BackColor = System.Drawing.Color.Transparent;
+            this.AA10.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.AA10.FlatAppearance.CheckedBackColor = System.Drawing.Color.Transparent;
+            this.AA10.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.AA10.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gainsboro;
+            this.AA10.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.AA10.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AA10.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.AA10.Location = new System.Drawing.Point(157, 186);
+            this.AA10.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.AA10.Name = "AA10";
+            this.AA10.Size = new System.Drawing.Size(121, 33);
+            this.AA10.TabIndex = 306;
+            this.AA10.Text = "Click here";
+            this.AA10.UseVisualStyleBackColor = false;
+            this.AA10.Click += new System.EventHandler(this.AA10_Click);
+            // 
+            // Help
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::BattleShip.Properties.Resources.background_2;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(452, 224);
+            this.Controls.Add(this.AA10);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Help";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Help";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button AA10;
+    }
+}
---
+++ BattleShip/Help.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace BattleShip
+{
+    public partial class Help : Form
+    {
+        public Help()
+        {
+            InitializeComponent();
+        }
+
+        private void AA10_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://www.wikihow.com/Play-Battleship");
+        }
+    }
+}
